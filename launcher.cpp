adde500e8fa6381fbf35f65ab00c3aff673be249
Tri Dao <tri.dao483@gmail.com>
2020-12-20 00:44:41 -0800

    feat: Implement User input
    Added in instructions methods to launcher. As well as taking in user input to decide
    which option to take.

--- launcher.cpp
+++ launcher.cpp
@@ -1,9 +1,36 @@
 // ------ Includes ------
 #include "Board.hpp"
+#include "Instructions.hpp"
+#include <limits>
 using namespace std;
 
 int main() {
-    Board b;
-    b.printBoard();
-    return 0;
+    Instructions instruct;
+    bool flag = true;
+    while (flag) {
+        instruct.startScreen();
+        cout << "Enter option by number.... ";
+        int start_option;
+        cin >> start_option;
+        // If 1 is entered, play game
+        if (start_option == 1) {
+
+        }
+        // If 2 is entered, print instructions
+        else if (start_option == 2) {
+            cout << endl;
+            instruct.howToPlay();
+            cout << endl;
+            while (cin.get() != 'q') {
+                cout << "Enter q to continue... ";
+            }
+        }
+        // If 3 is entered, quit the program
+        else if (start_option == 3) {
+            flag = false;
+        }
+        else {
+            cout << "Invalid Option. Try again using numbers 1-3" << endl;
+        }
+    }
 }
