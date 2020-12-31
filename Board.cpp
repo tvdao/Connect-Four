85a1a51454eae60f70c2831cf167268a32b7ee3d
Tri Dao <tri.dao483@gmail.com>
2020-12-31 03:02:08 -0800

    refactor: Change loop cond in check_win
    Loop starts at index 0 now rather than 1.

--- Board.cpp
+++ Board.cpp
@@ -215,8 +215,8 @@ bool Board::check_diagonal(int vert, int hor) {
  */
 bool Board::check_win() {
     // Check horizontal
-    for (int i = mBoard.size()-1; i > 0; i--) {
-        for (int j = 1; j < mBoard[i].size(); j++) {
+    for (int i = BOARD_HEIGHT-1; i > 0; i--) {
+        for (int j = 0; j < BOARD_WIDTH; j++) {
             if (j < 5) {
                 if (check_horizontal(i, j)) {
                     return true;
